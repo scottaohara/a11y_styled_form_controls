b86e6fb3299e6b9a8f3819389493c780fc91198e
Scott O'Hara <scottaohara@gmail.com>
2018-04-22 21:25:49 -0400

    make JS files consistent
    link to license
    make the two JS files consistent in how they’re structured.

--- src/assets/js/file-upload.js
+++ src/assets/js/file-upload.js
@@ -11,28 +11,30 @@
 	A11YfileUp.NS      = 'A11YfileUp';
 	A11YfileUp.AUTHOR  = 'Scott O\'Hara';
 	A11YfileUp.VERION  = '0.1.0';
-	A11YfileUp.LICENSE = '';
+	A11YfileUp.LICENSE = 'https://github.com/scottaohara/a11y_styled_form_controls/blob/master/LICENSE';
 
 	var widget = doc.querySelectorAll('[data-file-input]');
 
 	A11YfileUp.init = function () {
+		var self;
 		var i;
 
 		for ( i = 0; i < widget.length; i++ ) {
-			var label = widget[i].querySelector('.file-up__label');
-			var input = widget[i].querySelector('[type="file"]');
+			self = widget[i];
+			var label = self.querySelector('.file-up__label');
+			var input = self.querySelector('[type="file"]');
 			var output = doc.createElement('span');
+
 			output.classList.add('file-up__output');
 			output.setAttribute('aria-hidden', 'true');
 
 			label.appendChild(output);
 
-			if ( widget[i].querySelector('[disabled]') ) {
-				widget[i].classList.add('is-disabled');
+			if ( self.querySelector('[disabled]') ) {
+				self.classList.add('is-disabled');
 			}
 
 			input.addEventListener('change', function () {
-
 				if ( this.hasAttribute('multiple') && this.files.length > 1 ) {
 					this.parentNode.querySelector('.file-up__output').innerHTML = this.files.length + ' files selected.';
 				}
@@ -41,7 +43,7 @@
 				}
 			});
 		}
-	}; // A11YfileUp.create()
+	}; // A11YfileUp.init()
 
 	// go go JavaScript
 	A11YfileUp.init();
@@ -53,44 +55,46 @@
  * Focus within in all browsers.
  * source: https://gist.github.com/aFarkas/a7e0d85450f323d5e164
  */
-	(function ( w, doc ) {
-		'use strict';
+(function ( w, doc ) {
+	'use strict';
 
-		var slice = [].slice;
-		var removeClass = function (elem) {
-			elem.classList.remove('focus-within');
+	var slice = [].slice;
+
+	var removeClass = function (elem) {
+		elem.classList.remove('focus-within');
+	};
+
+	var update = ( function () {
+		var running
+		var last;
+
+		var action = function () {
+			var element = doc.activeElement;
+
+			running = false;
+
+			if ( last !== element ) {
+				last = element;
+				slice.call(doc.getElementsByClassName('focus-within')).forEach(removeClass);
+
+				while ( element && element.classList ) {
+					element.classList.add('focus-within');
+					element = element.parentNode;
+				}
+			}
 		};
 
-		var update = ( function () {
-			var running
-			var last;
+		return function () {
+			if (!running) {
+				requestAnimationFrame(action);
+				running = true;
+			}
+		};
+	})();
 
-			var action = function () {
-				var element = doc.activeElement;
+	doc.addEventListener('focus', update, true);
+	doc.addEventListener('blur', update, true);
 
-				running = false;
+	update();
 
-				if ( last !== element ){
-					last = element;
-					slice.call(doc.getElementsByClassName('focus-within')).forEach(removeClass);
-
-					while ( element && element.classList ) {
-						element.classList.add('focus-within');
-						element = element.parentNode;
-					}
-				}
-			};
-			return function () {
-				if (!running) {
-					requestAnimationFrame(action);
-					running = true;
-				}
-			};
-		})();
-
-		doc.addEventListener('focus', update, true);
-		doc.addEventListener('blur', update, true);
-		update();
-
-	})( window, document );
-
+})( window, document );
--- src/assets/js/switch-btn.js
+++ src/assets/js/switch-btn.js
@@ -1,33 +1,55 @@
 ;(function ( w, doc, undefined ) {
 	'use strict';
 
-	var peSwitch = document.querySelectorAll('[data-btn-switch]');
-	var i;
+	/**
+	 * Local object for method references
+	 * and define script meta-data
+	 */
+	var A11YswButtons = {};
+	w.A11YswButtons   = A11YswButtons;
+
+	A11YswButtons.NS      = 'A11YswButtons';
+	A11YswButtons.AUTHOR  = 'Scott O\'Hara';
+	A11YswButtons.VERION  = '0.1.0';
+	A11YswButtons.LICENSE = 'https://github.com/scottaohara/a11y_styled_form_controls/blob/master/LICENSE';
+
+	var widget = doc.querySelectorAll('[data-btn-switch]');
 
 	/**
 	 * Run function to progressively enhance a checkbox pattern
 	 * into an ARIA Button to act as a Switch Toggle.
 	 */
-	for ( i = 0; i < peSwitch.length; i++ ) {
-		var getLabel = peSwitch[i].querySelector('label').innerHTML;
-		var getID = peSwitch[i].querySelector('[type="checkbox"]').id;
-		peSwitch[i].innerHTML = '<button type="button" id="' + getID + '" class="btn-switch" aria-pressed="false"><span class="btn-switch__label">' + getLabel + '</span><span class="btn-switch__toggle"></span></button>';
-	}
+	A11YswButtons.init = function () {
+		var self;
+		var btn;
+		var i;
 
-	/**
-	 * After all buttons have been setup, search
-	 * DOM for buttons to attach toggle function to.
-	 */
-	var btn = document.querySelectorAll('.btn-switch');
+		for ( i = 0; i < widget.length; i++ ) {
+			self = widget[i];
+			var getLabel = self.querySelector('label').innerHTML;
+			var getID = self.querySelector('[type="checkbox"]').id;
 
-	/**
-	 * After all switches have been setup, setup
-	 * eventListenter for on/off functionality.
-	 */
-	for ( i = 0; i < btn.length; i++ ) {
-		btn[i].addEventListener('click', function ( e ) {
-			this.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
-		});
-	}
+			self.innerHTML = '<button type="button" id="' + getID + '" class="btn-switch" aria-pressed="false"><span class="btn-switch__label">' + getLabel + '</span><span class="btn-switch__toggle"></span></button>';
+		}
+
+		/**
+		 * After all buttons have been setup, search
+		 * DOM for buttons to attach toggle function to.
+		 */
+		btn = doc.querySelectorAll('.btn-switch');
+
+		/**
+		 * After all switches have been created, setup
+		 * eventListenter for on/off functionality.
+		 */
+		for ( i = 0; i < btn.length; i++ ) {
+			btn[i].addEventListener('click', function ( e ) {
+				this.setAttribute('aria-pressed', e.target.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
+			});
+		}
+	}; // A11YswButtons.init()
+
+	// go go JavaScript
+	A11YswButtons.init();
 
 })( window, document );
