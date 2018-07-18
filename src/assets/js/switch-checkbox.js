ffd6b4de24dbedc2c54ece05c3cd03da72643db2
Scott O'Hara <scottaohara@gmail.com>
2018-07-18 16:08:10 -0400

    update checkbox switch js

--- src/assets/js/switch-checkbox.js
+++ src/assets/js/switch-checkbox.js
@@ -16,30 +16,37 @@
 	var widget = doc.querySelectorAll('[type="checkbox"][data-switch]');
 
 	A11YswitchCheck.init = function () {
-		var enterKey = 13;
 		var self;
-		var keyCode;
 		var i;
 
 		for ( i = 0; i < widget.length; i++ ) {
 			self = widget[i];
 
 			self.setAttribute('role', 'switch');
+			self.setAttribute('aria-checked', 'false');
 
-			self.addEventListener('keydown', function ( e ) {
-				keyCode = e.keyCode || e.which;
-
-				switch ( keyCode ) {
-					case enterKey:
-					case 32:
-						e.preventDefault();
-						e.target.click();
-						e.target.focus(); // fix for ie?
-					break;
-				}
-			});
+			self.addEventListener('click', A11YswitchCheck.toggleState, false);
+			self.addEventListener('keypress', A11YswitchCheck.keyEvents, false);
 		}
 	}; // A11YswitchCheck.init()
+
+	A11YswitchCheck.keyEvents = function ( e ) {
+		var enterKey = 13;
+		var keyCode = e.keyCode || e.which;
+
+		switch ( keyCode ) {
+			case enterKey:
+			case 32:
+				e.preventDefault();
+				e.target.click();
+				e.target.focus(); // fix for ie?
+			break;
+		}
+	}
+
+	A11YswitchCheck.toggleState = function ( e ) {
+    this.setAttribute('aria-checked', this.getAttribute('aria-checked') === 'true' ? 'false' : 'true');
+	};
 
 	// go go JavaScript
 	A11YswitchCheck.init();
