ff74e7b25d685d2e33ad09c376930322c174cc93
Scott O'Hara <scottaohara@gmail.com>
2018-07-19 19:01:06 -0400

    documenting

--- src/assets/js/switch-toggle-button.js
+++ src/assets/js/switch-toggle-button.js
@@ -13,94 +13,112 @@
 	A11yToggleButton.VERION  = '0.1.0';
 	A11yToggleButton.LICENSE = 'https://github.com/scottaohara/a11y_styled_form_controls/blob/master/LICENSE';
 
-	var widget = doc.querySelectorAll('[data-toggle-btn]');
-
-	A11yToggleButton.setup = function () {
-		var self;
+	A11yToggleButton.init = function ( customSelector, buttonLabel ) {
+		var selectors = '[data-toggle-btn], ' + customSelector;
+		var self = doc.querySelectorAll(selectors);
 		var switchUI;
-		var i;
+	  var i = self.length;
 
-		for ( i = 0; i < widget.length; i++ ) {
-			self = widget[i];
-			switchUI = self.querySelector('span');
+	  for ( i = 0; i < self.length; i++ ) {
+	  	switchUI = self[i].querySelector('[data-toggle-btn-ui]') || self[i].querySelector('.toggle-switch__ui');
+	    setup(self[i], switchUI, buttonLabel);
+	  }
+	} // A11yToggleButton.init
 
-			/**
-			 * Check instances of the widget.
-			 * If the widget is not a button element, then add a role button.
-			 * If it is not an a[href], or already have a tabindex, then
-			 * provide a tabindex=0 so it can be keyboard focusable.
-			 */
-			if ( self.tagName !== 'BUTTON' ) {
-				self.setAttribute('role', 'button');
 
-				if ( !self.hasAttribute('href') && !self.hasAttribute('tabindex') ) {
-					self.tabIndex = 0;
-				}
+	var setup = function ( self, switchUI ) {
+		/**
+		 * Check instances of the widget.
+		 * If the widget is not a button element, then add a role button.
+		 * If it is not an a[href], or already have a tabindex, then
+		 * provide a tabindex=0 so it can be keyboard focusable.
+		 */
+		if ( self.tagName !== 'BUTTON' ) {
+			self.setAttribute('role', 'button');
+
+			if ( !self.hasAttribute('href') && !self.hasAttribute('tabindex') ) {
+				self.tabIndex = 0;
 			}
-
-			/**
-			 * If a button is missing an inner span to
-			 * wrap the accessible name and serve as the
-			 * basis for the switch UI, then take the contents
-			 * of the button and inject it into a newly created
-			 * span element.
-			 */
-			if ( !switchUI ) {
-				var newUI = doc.createElement('span');
-				self.append(newUI);
-				switchUI = self.querySelector('span');
-			}
-			switchUI.setAttribute('aria-hidden', 'true');
-
-			// if the default class for this component doesn't exist, add it
-			if ( !self.classList.contains('toggle-switch') ) {
-				self.classList.add('toggle-switch');
-			}
-
-			// if a switch ui should display the text 'on' and 'off'
-			if ( self.hasAttribute('data-toggle-btn-labels') ) {
-				self.classList.add('toggle-switch--labels');
-			}
-
-			if ( !switchUI.classList.contains('toggle-switch__ui') ) {
-				switchUI.classList.add('toggle-switch__ui');
-			}
-
-			/**
-			 * A toggle button is not particularly useful without JavaScript,
-			 * so ideally such a button would be set to disabled, if JS wasn't
-			 * around to make it function.
-			 *
-			 * Unless a toggle button is specifically meant to be disabled,
-			 * when JS is available, remove the disabled attribute so these
-			 * buttons can be used.
-			 */
-			if ( self.disabled && self.getAttribute('data-toggle-btn') !== "disabled" ) {
-				self.removeAttribute('disabled');
-			}
-
-			if ( !self.hasAttribute('aria-pressed') ) {
-
-				if ( self.hasAttribute('data-toggle-btn-pressed') ) {
-					self.setAttribute('aria-pressed', 'true');
-				}
-				else {
-					self.setAttribute('aria-pressed', 'false');
-				}
-			}
-
-			self.addEventListener('click', A11yToggleButton.toggleState, false);
-			self.addEventListener('keypress', A11yToggleButton.keyEvents, false);
 		}
-	}; // A11yToggleButton.setup
+
+		/**
+		 * If a button is missing an inner element to
+		 * wrap the accessible name and serve as the
+		 * basis for the switch UI, then create a span
+		 * and append it to the button element.
+		 */
+		if ( !switchUI ) {
+			var newUI = doc.createElement('span');
+			self.appendChild(newUI);
+			switchUI = self.querySelector('span');
+		}
+		switchUI.setAttribute('aria-hidden', 'true');
+
+		// if the default class for this component doesn't exist, add it
+		if ( !self.classList.contains('toggle-switch') ) {
+			self.classList.add('toggle-switch');
+		}
+
+		// if a switch ui should display the text 'on' and 'off'
+		if ( self.hasAttribute('data-toggle-btn-labels') ) {
+			self.classList.add('toggle-switch--labels');
+		}
+
+		if ( !switchUI.classList.contains('toggle-switch__ui') ) {
+			switchUI.classList.add('toggle-switch__ui');
+		}
+
+		/**
+		 * A toggle button is not particularly useful without JavaScript,
+		 * so ideally such a button would be set to disabled, if JS wasn't
+		 * around to make it function.
+		 *
+		 * Unless a toggle button is specifically meant to be disabled,
+		 * when JS is available, remove the disabled attribute so these
+		 * buttons can be used.
+		 */
+		if ( self.disabled && self.getAttribute('data-toggle-btn') !== "disabled" ) {
+			self.removeAttribute('disabled');
+		}
+
+		/**
+		 * If an element is set to be hidden, if JS is not available,
+		 * remove the hidden attribute
+		 */
+		if ( self.hidden ) {
+			self.removeAttribute('hidden');
+		}
+
+		/**
+		 * A toggle button won't register as a toggle button if an aria-pressed
+		 * isn't present prior to user interaction.
+		 *
+		 * e.g. a button that is pressed, and then acquires an aria-pressed='true'
+		 * attribute will NOT always be announced as a toggle button.
+		 *
+		 * Check for the presence of aria-pressed and if not there, run additional
+		 * checks to determine if this button should be set to true or false by default.
+		 */
+		if ( !self.hasAttribute('aria-pressed') ) {
+			self.setAttribute('aria-pressed', self.hasAttribute('data-toggle-btn-pressed'))
+		}
+
+		self.removeAttribute('data-toggle-btn')
+
+		/**
+		 * Event listeners for toggle buttons
+		 */
+		self.addEventListener('click', toggleState, false);
+		self.addEventListener('keypress', keyEvents, false);
+	}; // setup
 
 
-	A11yToggleButton.toggleState = function ( e ) {
+	var toggleState = function ( e ) {
     this.setAttribute('aria-pressed', this.getAttribute('aria-pressed') === 'true' ? 'false' : 'true');
-	}; // A11yToggleButton.toggleState
+	}; // toggleState
 
 
-	A11yToggleButton.keyEvents = function ( e ) {
+	var keyEvents = function ( e ) {
 		var keyCode = e.keyCode || e.which;
 
 		/**
@@ -121,19 +139,10 @@
 	        break;
 	    }
 		}
+	}; // keyEvents
 
-	}; // A11yToggleButton.keyEvents
 
-	/**
-   * Initialize functions.
-   * If expanding this script, place any other
-   * initialize functions within here.
-   */
-  A11yToggleButton.init = function () {
-    A11yToggleButton.setup();
-  };
-
-	// go go JavaScript
+	// Go go JavaScript
 	A11yToggleButton.init();
 
 })( window, document );
