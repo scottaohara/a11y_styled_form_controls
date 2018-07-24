4297fe824edf3b3728ca98f6ef520cd90a350ac1
Scott O'Hara <scottaohara@gmail.com>
2018-07-23 22:21:46 -0400

    revise fileupload script

--- src/assets/js/file-upload.js
+++ src/assets/js/file-upload.js
@@ -1,38 +1,57 @@
-;(function ( w, doc, undefined ) {
+(function ( w, doc, undefined ) {
 	'use strict';
+	var A11yFileUpload;
 
-	/**
-	 * Local object for method references
-	 * and define script meta-data
-	 */
-	var A11YfileUp = {};
-	w.A11YfileUp   = A11YfileUp;
+	A11yFileUpload = function ( ) {
+		/**
+		 * Author: Scott O'Hara
+		 * Version: 0.1.0
+		 * License: https://github.com/scottaohara/a11y_styled_form_controls/blob/master/LICENSE
+		 */
+		var el;
 
-	A11YfileUp.NS      = 'A11YfileUp';
-	A11YfileUp.AUTHOR  = 'Scott O\'Hara';
-	A11YfileUp.VERION  = '0.1.0';
-	A11YfileUp.LICENSE = 'https://github.com/scottaohara/a11y_styled_form_controls/blob/master/LICENSE';
+		/**
+		 * Initialize the instance, run all setup functions
+		 * and attach the necessary events.
+		 */
+		this.init = function ( elm ) {
+			el = elm;
+			setupPattern( el );
+			checkDisabled( el );
+			attachEvents( el );
+		};
 
-	var widget = doc.querySelectorAll('[data-file-input]');
+		/**
+		 * Setup the instance with n
+		 */
+		var setupPattern = function ( el ) {
+			var label = el.querySelector('.file-up__label');
 
-	A11YfileUp.init = function () {
-		var self;
-		var i;
-
-		for ( i = 0; i < widget.length; i++ ) {
-			self = widget[i];
-			var label = self.querySelector('.file-up__label');
-			var input = self.querySelector('[type="file"]');
 			var output = doc.createElement('span');
-
 			output.classList.add('file-up__output');
 			output.setAttribute('aria-hidden', 'true');
 
 			label.appendChild(output);
+		};
 
-			if ( self.querySelector('[disabled]') ) {
-				self.classList.add('is-disabled');
+		/**
+		 * Disable check
+		 * If an input is disabled, this pattern won't
+		 * dim everything. Set an is-disabled class to
+		 * make sure it visually looks disabled.
+		 */
+		var checkDisabled = function ( el ) {
+			if ( el.querySelector('[disabled]') ) {
+				el.classList.add('is-disabled');
 			}
+		}
+
+		/**
+		 * Events for toggle buttons
+		 * @return none
+		 */
+		var attachEvents = function ( el ) {
+			var input = el.querySelector('[type="file"]');
 
 			input.addEventListener('change', function () {
 				if ( this.hasAttribute('multiple') && this.files.length > 1 ) {
@@ -42,59 +61,10 @@
 					this.parentNode.querySelector('.file-up__output').innerHTML = this.value.replace(/(\w+:?[\\\/])/g, '');
 				}
 			});
-		}
-	}; // A11YfileUp.init()
-
-	// go go JavaScript
-	A11YfileUp.init();
-
-})( window, document );
-
-
-/**
- * Focus within in all browsers.
- * source: https://gist.github.com/aFarkas/a7e0d85450f323d5e164
- */
-(function ( w, doc ) {
-	'use strict';
-
-	var slice = [].slice;
-
-	var removeClass = function (elem) {
-		elem.classList.remove('focus-within');
-	};
-
-	var update = ( function () {
-		var running
-		var last;
-
-		var action = function () {
-			var element = doc.activeElement;
-
-			running = false;
-
-			if ( last !== element ) {
-				last = element;
-				slice.call(doc.getElementsByClassName('focus-within')).forEach(removeClass);
-
-				while ( element && element.classList ) {
-					element.classList.add('focus-within');
-					element = element.parentNode;
-				}
-			}
 		};
 
-		return function () {
-			if (!running) {
-				requestAnimationFrame(action);
-				running = true;
-			}
-		};
-	})();
+		return this;
+	}; // A11yFileUpload()
 
-	doc.addEventListener('focus', update, true);
-	doc.addEventListener('blur', update, true);
-
-	update();
-
+	w.A11yFileUpload = A11yFileUpload;
 })( window, document );
